a5c68b3950171a49f98709ec5ce8a6f555c0fc83
DESKTOP-5367GUT\오수빈 <eoghkqks@naver.com>
2021-07-10 00:46:49 +0900

    class rename runHackTool

--- RunHackTool.java
+++ RunHackTool.java
@@ -2,7 +2,7 @@ package miniProgram;
 
 import miniProgram.hackTool.Bruteforce;
 
-public class HackToolDependOnEngDict {
+public class RunHackTool {
 	public static void main(String[] args) {
 		
 		Bruteforce bf = new Bruteforce(new String("files/frankenstein_encrypted.txt"));
